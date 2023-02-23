a83c31e5199a490cc547765e246d47bbecacac72
Palich <y.tsaplay@yukonww.com>
2023-02-23 08:42:56 +0200

    searching for a black cat in a dark room.

--- src/main.c
+++ src/main.c
@@ -8,33 +8,10 @@
 LOG_MODULE_REGISTER(wfm200test, LOG_LEVEL_DBG);
 
 #include <zephyr/kernel.h>
-//#include <zephyr/device.h>
-//#include <zephyr/devicetree.h>
 
 #include <zephyr/net/wifi_mgmt.h>
 #include <zephyr/net/net_if.h>
 
-/*
-#include "sl_wfx.h"
-#include "sl_wfx_host_init.h"
-#include "sl_wfx_host.h"
-
-#include <zephyr/net/net_if.h>
-#include <zephyr/net/net_mgmt.h>
-*/
-// #if DT_NUM_INST_STATUS_OKAY(DT_DRV_COMPAT) == 0
-// #error"WM89xx series coder is not defined in DTS"
-// #endif
-
-// uint8_t buffer[4];
-
-// sl_wfx_context_t   wifi;
-
-// #define WIFI_NODE DT_NODELABEL(wifi)
-//#define WIFI_NODE DT_ALIAS(wifi0)
-//const struct device *const wifi_drv_0 = DEVICE_DT_GET(WIFI_NODE);
-
-// extern void gpio_setup(void);
 
 static struct net_mgmt_event_callback wifi_shell_mgmt_cb;
 static struct net_mgmt_event_callback  mgmt_if_cb;
@@ -103,21 +80,7 @@ static void my_if_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_
 
 void main(void)
 {
-//	struct net_if *iface = net_if_get_default();
-
-	LOG_INF("--- start ");
-
-//	if (!device_is_ready(wifi_drv_0)) {
-//		LOG_ERR("wfm200 devices not ready");
-//		return;
-//	}
-//	LOG_INF("wfm200 devices ready");
-	// k_sem_take(&wfx_init_sem,K_FOREVER);
-
-//	LOG_INF("device name = %s\n", wifi_drv_0->name);
-//
-//	iface = net_if_lookup_by_dev(wifi_drv_0);
-
+	LOG_INF("********************************** --- start ***********************************");
 	struct net_if * iface = net_if_get_first_by_type(&NET_L2_GET_NAME(ETHERNET));
 	if (iface) {
 		net_dhcpv4_start(iface);
@@ -130,39 +93,9 @@ void main(void)
 	net_mgmt_init_event_callback(&mgmt_if_cb, my_if_mgmt_event_handler, _NET_EVENT_IF_BASE);
 	net_mgmt_add_event_callback(&mgmt_if_cb);
 
-	// wfm200_scan();
-
-	// wfm200_mgmt_scan();
-	// wifi_scan();
-
-	// net_mgmt(NET_REQUEST_WIFI_SCAN, iface, NULL, 0);
-
-	// wifi_scan();
-	// printk("addr net_mgmt = %x\n");
-
-	/*
-	net_mgmt(NET_REQUEST_WIFI_SCAN, iface, NULL, 0);
-
-	k_msleep(6000);
-
-	net_mgmt(NET_REQUEST_WIFI_SCAN, iface, NULL, 0);
-	*/
-
 	while (1) {
-		k_msleep(100);
+		k_msleep(1000);
 	}
-	k_msleep(15000);
-	printk("--- end \n");
-
-	/**
-	if (!device_is_ready(wifi_drv)) {
-	    printk("wfm200 devices not ready\n");
-	    return;
-	}
-	*/
-
-	// TODO
-	//  net_mgmt_event_wait
 }
 
 // my comment
